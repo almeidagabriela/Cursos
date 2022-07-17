6520d2b7562daac204834d05ee4f801769f4c9c2
Gabriela Almeida <gabrielabrazolin@outlook.com>
2022-07-16 21:33:44 -0300

    adicionando validacao nas informacoes do filme

--- FilmesAPI/FilmesAPI/Models/Filme.cs
+++ FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -1,11 +1,19 @@
-﻿namespace FilmesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
 {
     public class Filme
     {
         // Informações do Filme
+
+        // Required: Validação de campo obrigatório com mensagem de erro especifica
+        [Required(ErrorMessage = "O campo título é obrigatório.")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatório.")]
         public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero não pode passar de 30 caracteres.")] // Definindo a quantidade máxima de caracteres
         public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos.")] // Validação de tamanho da duração com mensagem de erro especifica
         public int Duracao { get; set; }
     }
 }
