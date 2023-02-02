c8ffd5e4a4b043e088cd7a010c2aa98c5c40e994
Gabriela Almeida <gabrielabrazolin@outlook.com>
2023-02-01 22:25:54 -0300

    Relacionando endereco e cinema para o banco

--- FilmesAPI/FilmesAPI/Data/AppDbContext.cs
+++ FilmesAPI/FilmesAPI/Data/AppDbContext.cs
@@ -13,6 +13,16 @@ namespace FilmesAPI.Data
 
         }
 
+        // Explicitar que no momento da criação realizaremos algumas definições personalizadas
+        protected override void OnModelCreating(ModelBuilder builder) 
+        {
+            // Definições da Entidade do tipo Endereço
+            builder.Entity<Endereco>()
+                .HasOne(endereco => endereco.Cinema) // Determinando que um objeto do tipo endereço possui um cinema
+                .WithOne(cinema => cinema.Endereco) // O cinema possui um endereço
+                .HasForeignKey<Cinema>(cinema => cinema.EnderecoId); // Indicando que a chave estrangeira está na classe cinema pelo campo EnderecoId
+        }
+
         // Propriedade utilizando DbSet (responsavel pelo conjunto de dados para acesso ao banco)
         public DbSet<Filme> Filmes { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
