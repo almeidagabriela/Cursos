8cffd7210e3ca0bcaa2ed8b04d5dd7389c39af2c
Gabriela Almeida <gabrielabrazolin@outlook.com>
2023-02-04 18:07:33 -0300

    Ao deletar um Gerente a deleção será restrita (o padrão é cascata)

--- FilmesAPI/FilmesAPI/Data/AppDbContext.cs
+++ FilmesAPI/FilmesAPI/Data/AppDbContext.cs
@@ -25,7 +25,8 @@ namespace FilmesAPI.Data
             builder.Entity<Cinema>()
                 .HasOne(cinema => cinema.Gerente)
                 .WithMany(gerente => gerente.Cinemas) // O gerente pode ter de zero a muitos cinemas
-                .HasForeignKey(cinema => cinema.GerenteId);
+                .HasForeignKey(cinema => cinema.GerenteId)
+                .OnDelete(DeleteBehavior.Restrict); // Definindo que ao deletar um Gerente a deleção será restrita (o padrão é cascata)
         }
 
         // Propriedade utilizando DbSet (responsavel pelo conjunto de dados para acesso ao banco)
